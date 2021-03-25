a332393a0767a76f84b308c86f61afe8ca2c9c93
SimonLeclere <simon-leclere@orange.fr>
2021-03-25 16:01:18 +0100

    ✨

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-http.listen(80, () => console.log('Server running on port 80'));
+http.listen(5000, () => console.log('Server running on port 5000'));
 
 io.on('connection', (socket) => {
 	socket.on('draw', (data) => socket.broadcast.emit('draw', { ...data }));
